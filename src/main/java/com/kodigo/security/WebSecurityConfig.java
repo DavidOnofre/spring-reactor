5cd844d4b4851763ecc607611c3bb2184124ec3e
DavidOnofre <donofre@gmail.com>
2023-06-01 01:15:11 -0500

    Spring Security - 403 Mensaje

--- src/main/java/com/kodigo/security/WebSecurityConfig.java
+++ src/main/java/com/kodigo/security/WebSecurityConfig.java
@@ -3,15 +3,21 @@ package com.kodigo.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+
+import java.nio.charset.StandardCharsets;
 
 // Class s7
 @Configuration
@@ -34,9 +40,19 @@ public class WebSecurityConfig {
                 .exceptionHandling()
                 .authenticationEntryPoint((swe, e) -> Mono.fromRunnable(() -> {
                     swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                })).accessDeniedHandler((swe, e) -> Mono.fromRunnable(() -> {
-                    swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
                 }))
+                .accessDeniedHandler((swe, e) -> {
+                    swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                    swe.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                    String message = """
+                            {"message" : "You don't have permissions" }
+                            """;
+                    byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
+                    DataBuffer buffer = swe.getResponse().bufferFactory().wrap(bytes);
+                    return swe.getResponse().writeWith(Flux.just(buffer))
+                            .then();
+
+                })
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
